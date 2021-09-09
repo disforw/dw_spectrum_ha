03acab89fc7bdb451a3111363a96a0d29daf7011
disforw <github@benzona.net>
2021-09-09 10:41:07 -0400

    Update sensor.py

--- custom_components/dw_spectrum/sensor.py
+++ custom_components/dw_spectrum/sensor.py
@@ -1,21 +1,20 @@
 """GitHub sensor platform."""
+from datetime import timedelta
 import logging
 import re
-from datetime import timedelta
 from typing import Any, Callable, Dict, Optional
-from urllib import parse
 
-import gidgethub
-import voluptuous as vol
 from aiohttp import ClientError
+import gidgethub
 from gidgethub.aiohttp import GitHubAPI
-
+from homeassistant import config_entries, core
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
-    CONF_ADDRESS,
-    CONF_PORT,
-    CONF_USERNAME,
-    CONF_PASSWORD,
+    ATTR_NAME,
+    CONF_ACCESS_TOKEN,
+    CONF_NAME,
+    CONF_PATH,
+    CONF_URL,
 )
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
@@ -25,43 +24,59 @@ from homeassistant.helpers.typing import (
     DiscoveryInfoType,
     HomeAssistantType,
 )
+import voluptuous as vol
 
 from .const import (
-    BASE_API_URL,
-    DEFAULT_PORT,
+    ATTR_CLONES,
+    ATTR_CLONES_UNIQUE,
+    ATTR_FORKS,
+    ATTR_LATEST_COMMIT_MESSAGE,
+    ATTR_LATEST_COMMIT_SHA,
+    ATTR_LATEST_OPEN_ISSUE_URL,
+    ATTR_LATEST_OPEN_PULL_REQUEST_URL,
+    ATTR_LATEST_RELEASE_TAG,
+    ATTR_LATEST_RELEASE_URL,
+    ATTR_OPEN_ISSUES,
+    ATTR_OPEN_PULL_REQUESTS,
+    ATTR_PATH,
+    ATTR_STARGAZERS,
+    ATTR_VIEWS,
+    ATTR_VIEWS_UNIQUE,
+    CONF_REPOS,
+    DOMAIN,
 )
-
 
 _LOGGER = logging.getLogger(__name__)
 # Time between updating data from GitHub
 SCAN_INTERVAL = timedelta(minutes=10)
 
+REPO_SCHEMA = vol.Schema(
+    {vol.Required(CONF_PATH): cv.string, vol.Optional(CONF_NAME): cv.string}
+)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
-        vol.Required(CONF_ADDRESS): cv.string,
-        vol.Required(CONF_USERNAME): cv.string,
-        vol.Required(CONF_PASSWORD): cv.string,
-        vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
+        vol.Required(CONF_ACCESS_TOKEN): cv.string,
+        vol.Required(CONF_REPOS): vol.All(cv.ensure_list, [REPO_SCHEMA]),
+        vol.Optional(CONF_URL): cv.url,
     }
 )
 
-LINK_RE = re.compile(
-    r"\<(?P<uri>[^>]+)\>;\s*" r'(?P<param_type>\w+)="(?P<param_value>\w+)"(,\s*)?'
-)
 
-
-def get_last_page_url(link: Optional[str]) -> Optional[str]:
-    # https://developer.github.com/v3/#pagination
-    # https://tools.ietf.org/html/rfc5988
-    if link is None:
-        return None
-    for match in LINK_RE.finditer(link):
-        if match.group("param_type") == "rel":
-            if match.group("param_value") == "last":
-                return match.group("uri")
-    else:
-        return None
+async def async_setup_entry(
+    hass: core.HomeAssistant,
+    config_entry: config_entries.ConfigEntry,
+    async_add_entities,
+):
+    """Setup sensors from a config entry created in the integrations UI."""
+    config = hass.data[DOMAIN][config_entry.entry_id]
+    # Update our config to include new repos and remove those that have been removed.
+    if config_entry.options:
+        config.update(config_entry.options)
+    session = async_get_clientsession(hass)
+    github = GitHubAPI(session, "requester", oauth_token=config[CONF_ACCESS_TOKEN])
+    sensors = [GitHubRepoSensor(github, repo) for repo in config[CONF_REPOS]]
+    async_add_entities(sensors, update_before_add=True)
 
 
 async def async_setup_platform(
@@ -137,21 +152,20 @@ class GitHubRepoSensor(Entity):
             self.attrs[ATTR_LATEST_COMMIT_MESSAGE] = latest_commit["commit"]["message"]
             self.attrs[ATTR_LATEST_COMMIT_SHA] = latest_commit["sha"]
 
-            prs_url = f"/repos/{self.repo}/pulls"
-            prs_data = await self.github.getitem(
-                prs_url, {"state": "open", "sort": "created", "per_page": 1}
-            )
-            self.attrs[ATTR_OPEN_PULL_REQUESTS] = await self._get_total(prs_url)
-            if prs_data:
-                self.attrs[ATTR_LATEST_OPEN_PULL_REQUEST_URL] = prs_data[0]["html_url"]
+            # Using the search api to fetch open PRs.
+            prs_url = f"/search/issues?q=repo:{self.repo}+state:open+is:pr"
+            prs_data = await self.github.getitem(prs_url)
+            self.attrs[ATTR_OPEN_PULL_REQUESTS] = prs_data["total_count"]
+            if prs_data and prs_data["items"]:
+                self.attrs[ATTR_LATEST_OPEN_PULL_REQUEST_URL] = prs_data["items"][0][
+                    "html_url"
+                ]
 
             issues_url = f"/repos/{self.repo}/issues"
-            issues_data = await self.github.getitem(
-                issues_url, {"state": "open", "sort": "created", "per_page": 1}
-            )
+            issues_data = await self.github.getitem(issues_url)
             # GitHub issues include pull requests, so to just get the number of issues,
             # we need to subtract the total number of pull requests from this total.
-            total_issues = await self._get_total(issues_url)
+            total_issues = repo_data["open_issues_count"]
             self.attrs[ATTR_OPEN_ISSUES] = (
                 total_issues - self.attrs[ATTR_OPEN_PULL_REQUESTS]
             )
@@ -171,24 +185,6 @@ class GitHubRepoSensor(Entity):
             self._available = True
         except (ClientError, gidgethub.GitHubException):
             self._available = False
-            _LOGGER.exception("Error retrieving data from GitHub.")
-
-    async def _get_total(self, url: str) -> int:
-        """Get the total number of results for a GitHub resource URL.
-
-        GitHub's API doesn't provide a total count for paginated resources.  To get
-        around that and to not have to request every page, we do a single request
-        requesting 1 item per page.  Then we get the url for the last page in the
-        response headers and parse the page number from there.  This page number is
-        the total number of results.
-        """
-        api_url = f"{BASE_API_URL}{url}"
-        params = {"per_page": 1, "state": "open"}
-        headers = {"Authorization": self.github.oauth_token}
-        async with self.github._session.get(
-            api_url, params=params, headers=headers
-        ) as resp:
-            last_page_url = get_last_page_url(resp.headers.get("Link"))
-            if last_page_url is not None:
-                return int(dict(parse.parse_qsl(last_page_url))["page"])
-        return 0
+            _LOGGER.exception(
+                "Error retrieving data from GitHub for sensor %s.", self.name
+            )
